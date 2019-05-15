afa95c288bf9b87dca7b14a79ceeb8e53effbd64
zhangxudong21 <zhangxudong6@jd.com>
2019-05-15 11:21:49 +0800

    e

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// extends: ['@commitlint/config-conventional'],
 	rules: {
-		'type-empty': [2, 'always'],
+		'type-empty': [2, 'never'],
         'type-enum': [2, 'always', [
             "feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
         ]],
